92bdbd67420ad60c5ed5c1da4f11c746703b0262
Mahmoud Ali <mahmoudsallam2111@gmail.com>
2023-06-17 14:50:34 +0300

    upload v6.0

---
+++ ISP.DAL/Data/Models/ModelsCongiguration/BranchConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP.DAL.Data.Models.ModelsCongiguration
+{
+    public class BranchConfig : IEntityTypeConfiguration<Branch>
+    {
+        public void Configure(EntityTypeBuilder<Branch> entity)
+        {
+            entity
+                 .HasAlternateKey(e => e.Phone1);
+            entity
+             .HasAlternateKey(e => e.Phone2);
+            entity
+            .HasAlternateKey(e => e.Mobile1);
+            entity
+            .HasAlternateKey(e => e.Mobile2);
+
+        }
+
+
+
+    }
+}
