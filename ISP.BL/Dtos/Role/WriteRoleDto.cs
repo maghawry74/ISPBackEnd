7396c6b0c7038c38951143e0e197fd35b7e5f7a1
gehad-abdelmonem <gehadabdelmonam@gmail.com>
2023-06-23 03:20:37 +0300

    v8.3

--- ISP.BL/Dtos/Role/WriteRoleDto.cs
+++ ISP.BL/Dtos/Role/WriteRoleDto.cs
@@ -4,7 +4,16 @@ namespace ISP.BL.Dtos.Role
     public class WriteRoleDto
     {       
         public string Name { get; set; } = string.Empty;
-        public List<string> RolePermissions { get; set; } = new List<string>();
+        public List<string> claims { get; set; } = new List<string>();
+
+
+        //public List<ReadRolePermissions> RolePermissions { get; set; } = new List<ReadRolePermissions>();
+    }
+    public class updateRoleDto
+    {
+        public string id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<string> claims { get; set; } = new List<string>();
 
 
         //public List<ReadRolePermissions> RolePermissions { get; set; } = new List<ReadRolePermissions>();
